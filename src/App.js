501d253d4b43d4f671b115c39cfc766094e96600
sannusingh16 <sannusingh1509@gmail.com>
2021-05-15 17:16:34 +0530

    created board and square

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React from 'react';
+import Board from './Components/Board';
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+
+const App = () => {
+  return (
+    <div>
+      <h1>TIC-TOE</h1>
+      <Board />
+      
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = (props) => {
+  return (
+    <button type='button'>{props.value}</button>
+  )
+}
+
+export default Square
+
+
